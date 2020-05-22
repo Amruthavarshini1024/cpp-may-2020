68b45ada86d3fbe22228cf34d5764ed29f0fefac
Amruthavarshini1024 <65034834+Amruthavarshini1024@users.noreply.github.com>
2020-05-22 09:34:00 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int prime(int x);
 int main()
 { int n;
- int i;
+ int i; 
+ int check=0;
  cout<<"Enter a positive integer:";
  cin>>n;
- check=0;
+ 
  for(i=2;i<=n/2;++i) 
     { if(prime(i)==1) //check if i is prime
         { if(prime(n-i)==1) //check if n-i is prime
@@ -20,17 +21,18 @@ int main()
    
    { cout<<n<<"can not be expressed as a sum of two prime numbers";}
    
-   return0;
+   return 0;
    }
    
-   int prime(int x)
+   int prime(int x) 
    {
      int p; 
      bool isPrime=1; 
      for(p=2;p<=x/2;++p)
-       { if(n%p==0) {isPrime=0; break;}
+       { if(x%p==0) {isPrime=0; //if x is not a prime number, prime func returns 0
+                     break;}
        }
-   return isPrime;
+   return isPrime; //isPrime==1 implies x is prime, else not prime
    }
        
     
